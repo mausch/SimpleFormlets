d1e1bbc02c8524dc15db0226a98bf7480b004c73
mausch <mauricioscheffer@gmail.com>
2011-01-14 21:04:54 -0300

    initial commit - bare web project

---
+++ SimpleFormlets/Controllers/HomeController.fs
@@ -0,0 +1,13 @@
+﻿namespace FSharpMVC2.Web.Controllers
+
+open System.Web
+open System.Web.Mvc
+
+[<HandleError>]
+type HomeController() =
+    inherit Controller()
+    member x.Index () : ActionResult =
+        x.ViewData.["Message"] <- "Welcome to ASP.NET MVC!"
+        x.View() :> ActionResult
+    member x.About () =
+        x.View() :> ActionResult
---
+++ SimpleFormlets/Core/Global.fs
@@ -0,0 +1,27 @@
+﻿namespace FSharpMVC2.Core
+
+open System
+open System.Web
+open System.Web.Mvc
+open System.Web.Routing
+
+type Route = { 
+    controller : string
+    action : string
+    id : UrlParameter }
+
+type Global() =
+    inherit System.Web.HttpApplication() 
+
+    static member RegisterRoutes(routes:RouteCollection) =
+        routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
+        routes.MapRoute(
+            "Default", // Route name
+            "{controller}/{action}/{id}", // URL with parameters
+            { controller = "Home"; action = "Index"; id = UrlParameter.Optional } // Parameter defaults
+            )
+
+    member x.Start() =
+        AreaRegistration.RegisterAllAreas()
+        Global.RegisterRoutes(RouteTable.Routes)
+
